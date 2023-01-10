cc4b1f0bf16ffbd565eacd1aae8ae43040dfb011
Lyudmila Maximenko <109598383+LyudmilaMax@users.noreply.github.com>
2023-01-11 01:38:10 +0200

    done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,23 @@
+// Імпорт для відображення повідомлень користувачеві
 import Notiflix from 'notiflix';
 
+// Звернутися до розмітки HTML
 const formEl = document.querySelector('.form');
 const delayEl = formEl.querySelector('[name="delay"]');
 const stepEl = formEl.querySelector('[name="step"]');
 const amountEl = formEl.querySelector('[name="amount"]');
 
+// Оголошенні змінні ключів веб-сховища LocalStorаge
 let keyDelay = 'PROMISE_DELAY';
 let keyStep = 'PROMISE_STEP';
 let keyAmount = 'PROMISE_AMOUNT';
 
+// Ініціалізація значеннями останнього введеня (збереженними в LocalStorage)
 delayEl.value = Number(localStorage.getItem(keyDelay));
 stepEl.value = Number(localStorage.getItem(keyStep));
 amountEl.value = Number(localStorage.getItem(keyAmount));
 
+// Зберегти значення в LocalStorage
 delayEl.addEventListener('input', evt => {
   localStorage.setItem(keyDelay, evt.target.value);
 });
@@ -24,8 +29,10 @@ amountEl.addEventListener('input', evt => {
   localStorage.setItem(keyAmount, evt.target.value);
 });
 
+//Додати тип події "submit" на кнопку "Create promises"
 formEl.addEventListener('submit', onSubmitCreatePromise);
 
+// ФУНКЦІЯ. Створення всіх промісів по введених параметрах
 function onSubmitCreatePromise(evt) {
   evt.preventDefault();
   let delayValue = Number(delayEl.value);
