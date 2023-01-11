668f0bbd14ad4964477477c6703c0c711a5d487c
Lyudmila Maximenko <109598383+LyudmilaMax@users.noreply.github.com>
2023-01-11 23:37:49 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,40 +7,30 @@ const delayEl = formEl.querySelector('[name="delay"]');
 const stepEl = formEl.querySelector('[name="step"]');
 const amountEl = formEl.querySelector('[name="amount"]');
 
-// Оголошенні змінні ключів веб-сховища LocalStorаge
-let keyDelay = 'PROMISE_DELAY';
-let keyStep = 'PROMISE_STEP';
-let keyAmount = 'PROMISE_AMOUNT';
-
-// Ініціалізація значеннями останнього введеня (збереженними в LocalStorage)
-delayEl.value = Number(localStorage.getItem(keyDelay));
-stepEl.value = Number(localStorage.getItem(keyStep));
-amountEl.value = Number(localStorage.getItem(keyAmount));
-
-// Зберегти значення в LocalStorage
-delayEl.addEventListener('input', evt => {
-  localStorage.setItem(keyDelay, evt.target.value);
-});
-stepEl.addEventListener('input', evt => {
-  localStorage.setItem(keyStep, evt.target.value);
-});
-
-amountEl.addEventListener('input', evt => {
-  localStorage.setItem(keyAmount, evt.target.value);
-});
-
 //Додати тип події "submit" на кнопку "Create promises"
 formEl.addEventListener('submit', onSubmitCreatePromise);
 
 // ФУНКЦІЯ. Створення всіх промісів по введених параметрах
 function onSubmitCreatePromise(evt) {
   evt.preventDefault();
+
   let delayValue = Number(delayEl.value);
   let stepValue = Number(stepEl.value);
   let amountValue = Number(amountEl.value);
-
+  evt.target.reset();
   for (let i = 1; i <= amountValue; i += 1) {
-    createPromise(i, delayValue);
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+
     delayValue += stepValue;
   }
 }
@@ -60,12 +50,5 @@ function createPromise(position, delay) {
       }
     }, delay);
   });
-
-  promise
-    .then(({ position, delay }) => {
-      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
+  return promise;
 }
